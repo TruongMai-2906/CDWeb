3d0763136c1ad12ac4da45dea977ed3a021d7125
TruongMai-2906 <truongbe2906@gmail.com>
2022-05-15 14:54:07 +0700

    -add: axios setup

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './HomePage.module.scss';
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
@@ -12,68 +12,84 @@ import Banner from '../../assets/images/home/banner1.jpg';
 import Banner2 from '../../assets/images/home/banner2.jpg';
 import Bannerctv from '../../assets/images/home/bannerctv.jpg';
 import { Link } from 'react-router-dom';
-
+import { get } from '../../utilities/api.ts';
 
 export interface HomePageProps { }
 
 export interface HomePageDataType { }
 
-const HomePage: React.FC<HomePageProps> = (props) => (
-  <div className={styles['root']}>
-    <Swiper
-      direction={"vertical"}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className={styles["swiper"]}
-    >
-      <SwiperSlide>
-        <div className={styles["item"]}>
-        <img src={Bannerctv} alt="banner" className={styles['background']} />
-        <div className={styles['content']}>
-          <Link to={'/detail'}>Công tố viên chuyển sinh</Link>
-        </div>
-      </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className={styles["item"]}>
-          <img src={Banner2} alt="banner" className={styles['background']} />
+const HomePage: React.FC<HomePageProps> = (props) => {
+  
+  const [data, setData] = useState();
+  useEffect( () => {
+    
+    if (!data) get('https://jsonplaceholder.typicode.com/todos/1').then((res) => setData(res.data));
+   ;
+  }, [])
+
+  useEffect( () => {
+    
+    if (data) console.log(data);
+    
+  }, [data])
+
+  return (
+    <div className={styles['root']}>
+      <Swiper
+        direction={"vertical"}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className={styles["swiper"]}
+      >
+        <SwiperSlide>
+          <div className={styles["item"]}>
+          <img src={Bannerctv} alt="banner" className={styles['background']} />
           <div className={styles['content']}>
-            this is content
+            <Link to={'/detail'}>Công tố viên chuyển sinh</Link>
           </div>
         </div>
-      </SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>
-        <div className={styles["item"]}>
-          <img src={Banner} alt="banner" className={styles['background']} />
-          <div className={styles['content']}>
-            <Link to={'/detail'}>Phù Thủy Strange</Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles["item"]}>
+            <img src={Banner2} alt="banner" className={styles['background']} />
+            <div className={styles['content']}>
+              this is content
+            </div>
           </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>
-        <div className={styles["item"]}>
-          <img src={Banner} alt="banner" className={styles['background']} />
-          <div className={styles['content']}>
-            this is content
+        </SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>
+          <div className={styles["item"]}>
+            <img src={Banner} alt="banner" className={styles['background']} />
+            <div className={styles['content']}>
+              <Link to={'/detail'}>Phù Thủy Strange</Link>
+            </div>
           </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className={styles["item"]}>
-          <img src={Banner2} alt="banner" className={styles['background']} />
-          <div className={styles['content']}>
-            <Link to={'/detail'}>Phù Thủy Strange</Link>
+        </SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>
+          <div className={styles["item"]}>
+            <img src={Banner} alt="banner" className={styles['background']} />
+            <div className={styles['content']}>
+              this is content
+            </div>
           </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
-      <SwiperSlide>Slide 9</SwiperSlide>
-    </Swiper>
-  </div>
-);
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles["item"]}>
+            <img src={Banner2} alt="banner" className={styles['background']} />
+            <div className={styles['content']}>
+              <Link to={'/detail'}>Phù Thủy Strange</Link>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </div>
+  )
+};
 
 export default HomePage;
