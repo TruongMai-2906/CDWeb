80a7296410234ede090650cf44ff6a3087d9b907
trung9266 <18130259@st.hcmuaf.edu.vn>
2022-05-15 11:44:19 +0700

    Page Watch Movie

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Footer from './components/Footer/Footer.tsx';
 //@ts-ignore
 import Detail from './pages/Detail/Detail.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import WatchFilm from "./pages/WatchFilm/WatchFilm.tsx";
 function App() {
   return (
     <div className="App">
@@ -21,6 +22,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/detail" element={<Detail/>}></Route>
+        <Route path="/watch" element={<WatchFilm/>}/>
       </Routes>
       <Footer></Footer>
     </Router>
